fbfd288bc9b83e30ea2fb90883f10968154766d7
Peter (Somogyvari) Metz <peter.metz@unarin.com>
2018-08-07 16:08:17 -0700

    chore(@postgres-hive-open-xa) adds main class, sets up IDE files

---
+++ src/main/java/com/unarin/poc/persistence/ProofOfConceptMain.java
@@ -0,0 +1,8 @@
+package com.unarin.poc.persistence;
+
+public class ProofOfConceptMain {
+
+    public static void main(String[] args) {
+        System.out.println("Proof of concept - PostgreSQL Hive Open/XA");
+    }
+}
